4114eef4e0ec4b581f7ddbb5b0292321afc532c5
sid8808 <samsid8808@gmail.com>
2023-02-17 13:01:21 +0530

    Navbar Initial

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,11 @@
-import logo from './logo.svg';
-import './App.css';
+import Navbar from "./Components/Navbar/Navbar";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
+      <Navbar />
     </div>
   );
 }
